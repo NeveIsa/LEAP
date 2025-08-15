b8d698bd56476fbd99953daa01736a9f00a9e361
smpdmohanty <smpdmohanty@gmail.com>
2025-08-15 12:56:33 -0700

    updated

--- demo/2_eigen.py
+++ demo/2_eigen.py
@@ -17,12 +17,12 @@ def powerMethod(A, maxIterations = 1000, tol = 1e-10):
     previousLam = 0 #used as reference to check if eigenvalue is close enough to the tol
 
     for _ in range(maxIterations):
-        y = np.array(A(x)) #applying transformation to x
+        y = np.array(A(x.tolist())) #applying transformation to x
         normY = np.linalg.norm(y) #normalizing vector to have lenght 1 (scaling purposes)
         if normY == 0: #if length of y is already equal to zero 
             return 0, x #eigen value is 0 @ this x
         x = y / normY #setting x to normalized length y (for finding the next approximation)
-        lam = x @ np.array(A(x)) #computing the rayleigh quotient to find the eigenvalue
+        lam = x @ np.array(A(x.tolist())) #computing the rayleigh quotient to find the eigenvalue
         if abs(lam - previousLam) < tol * (1 + abs(lam)): #comparing the change in the eigenvalue to see if it has converged, then we can assume the vector is stabilized
             return lam, x
         previousLam = lam
@@ -39,17 +39,22 @@ def deflation(A, lam, v):
 
     def B(x):
         x = np.asarray(x, dtype=float).reshape(-1)
-        Ax = np.asarray(A(x), dtype=float)
+        Ax = np.asarray(A(x.tolist()), dtype=float)
         return (Ax - lam * v * (v @ x)).tolist()
     return B
 
-k, v1 = powerMethod(client.linear)
-print("Eigenvalue 1: ", k)
-print("Eigenvector 1: ", v1)
 
-B = deflation(client.linear, k, v1)
+if __name__ == "__main__":
+    client = RPCClient(server_url=SERVER_URL, student_id=STUDENT_ID, experiment_name=EXPERIMENT)
 
-u, v2 = powerMethod(B)
-print("Eigenvalue 2: ", u)
-print("Eigenvector 2: ", v2)
+
+    k, v1 = powerMethod(client.linear)
+    print("Eigenvalue 1: ", k)
+    print("Eigenvector 1: ", v1)
+
+    B = deflation(client.linear, k, v1)
+
+    u, v2 = powerMethod(B)
+    print("Eigenvalue 2: ", u)
+    print("Eigenvector 2: ", v2)
 
